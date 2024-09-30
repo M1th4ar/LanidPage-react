d1fbff9fa36dd87e60d2b57e318940e444a42fa9
M1th4ar <harrydz2003@gmail.com>
2024-09-30 20:02:52 +0100

    removed-dist

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,13 +11,13 @@ const NavBar = () => {
   };
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-between item-center">
+      <div className="container px-4 mx-auto relative lg:text-sm">
+        <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="" />
             <span className="text-xl tracking-tight">VirtualR</span>
           </div>
-          <ul className="hidden lg:flex ml-14 space-x-12 items-center">
+          <ul className="hidden lg:flex ml-14 space-x-12 ">
             {navItems.map((item, index) => (
               <li key={index}>
                 <a href={item.href}> {item.label} </a>
@@ -35,7 +35,7 @@ const NavBar = () => {
               Create an account
             </a>
           </div>
-          <div className="lg:hidde md:flex flex-col justify-end">
+          <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? <X /> : <Menu />}
             </button>
