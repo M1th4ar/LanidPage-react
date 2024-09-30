c2e7a3b20a4be68c48cd92e835c8f74c3d117f1d
M1th4ar <harrydz2003@gmail.com>
2024-09-30 17:06:31 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 import NavBar from "./components/navBar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
 
 const App = () => {
   return (
- <NavBar />
+    <>
+      <NavBar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
   );
 };
 
